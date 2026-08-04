987410d26876f93678de8d1aef0626adb8e1f4dc
keyner Arismendy <keinerf588@gmail.com>
2026-08-04 17:21:47 -0500

    consumo de apis con html sencillo

---
+++ frontend/scripts/script.js
@@ -0,0 +1,23 @@
+var dir = document.getElementById('show-api');
+
+
+console.log(dir);
+
+var api = document.createElement('p');
+
+
+dir.append(api);
+
+
+
+async function get() {
+    var response = await fetch('127.0.0.1:1337');
+
+    var data = await response.json();
+
+    return data;
+
+}
+
+
+dir.innerHTML = get()
