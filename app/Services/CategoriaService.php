31b88f055afadba10809ff6b68982dbe3cdde67a
s48098801@gmail.com <s48098801@gmail.com>
2026-08-16 21:54:34 -0500

    category services finalizado

--- app/Services/CategoriaService.php
+++ app/Services/CategoriaService.php
@@ -20,6 +20,10 @@ class CategoriaService
         if(trim($categoria->getNombreCategoria()) === "") {
             throw new Exception("El nombre es obligatorio");
         }
+
+        if(trim($categoria->getDetalleCategoria()) ==="") {
+            throw new Exception("El detalle de la categoria es obligatorio");
+        }
     }
 
    public function guardar(Categoria $categoria): void
@@ -34,4 +38,40 @@ class CategoriaService
 
     $this->categoriaRepository->guardar($categoria);
 }
+   public function obtenerPorId(int $id): Categoria
+{
+    $categoria = $this->categoriaRepository->obtenerPorId($id);
+
+    if($categoria === null) {
+        throw new Exception("La categoria no existe.");
+    }
+
+    return $categoria;
+}
+   
+   public function actualizar(Categoria $categoria): void
+{
+    $this->obtenerPorId($categoria->getId());
+
+    $this->validarCategoria($categoria);
+
+    if($this->categoriaRepository->existeNombreCategoriaPorId(
+        $categoria->getNombreCategoria(),
+        $categoria->getId()
+    )) {
+        throw new Exception("Ya existe una categoria con este nombre");
+    }
+
+    $this->categoriaRepository->actualizar($categoria);
+}
+
+
+  public function eliminar(int $id): void
+{
+    $this->obtenerPorId($id);
+
+    $this->categoriaRepository->eliminar($id);
+}
+
+
 }
